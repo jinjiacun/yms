fe7c133205bc32fdc0dd7ed089f320500f6fa458
jinjiacun <jinjiacuncucu@sina.com>
2015-01-15 17:20:45 +0800

    new file:   Soadmin/Controller/InexposalController.class.php 	new file:   Soadmin/View/Comment/get_list.html 	new file:   Soadmin/View/Company/add.html 	new file:   Soadmin/View/Company/edit.html 	new file:   Soadmin/View/Company/get_list.html 	new file:   Soadmin/View/Inexposal/get_list.html 	new file:   Soadmin/View/Inexposal/get_list_ex.html 	new file:   Soadmin/View/News/add_ex.html 	new file:   Soadmin/View/News/edit_ex.html 	new file:   Soadmin/View/News/get_list_ex.html

---
+++ App/Soadmin/Controller/InexposalController.class.php
@@ -0,0 +1,91 @@
+<?php
+namespace Soadmin\Controller;
+use Think\Controller;
+include_once(dirname(__FILE__).'/BaseController.class.php');
+/**
+ *入库管理
+*/
+class InexposalController extends BaseController {
+    
+    //曝光
+    public function get_list()
+    {
+        $page_index = 1;
+        $page_size  = 10;
+        $content    = array();
+        if(I('get.p'))
+        {
+            $page_index = I('get.p');
+            $content['page_size'] = $page_size;
+            $content['page_index'] = $page_index;
+        }
+        $content['where'] = array(
+            'type'=>0,
+        );
+        $res = A('Callapi')->call_api('Inexposal.get_list', 
+                                    $content,
+                                    'text',
+                                  null);
+        $result = $this->deal_re_call_api($res);
+
+        $list = array();
+        if($result)
+        {
+            if(200 == $result['status_code'])
+            {
+                if(isset($result['content']['list'])
+                && isset($result['content']['record_count']))
+                {
+                    $list   = $result['content']['list'];   
+                    $this->assign('list', $list);     
+                    $record_count = $result['content']['record_count'];
+                    $this->assign('record_count', $record_count);
+                    $this->get_page($record_count, 10);
+                }
+            }
+        }
+        
+        $this->display();
+    }
+    
+    //认证申请
+    public function get_list_ex()
+    {
+         $page_index = 1;
+        $page_size  = 10;
+        $content    = array();
+        if(I('get.p'))
+        {
+            $page_index = I('get.p');
+            $content['page_size'] = $page_size;
+            $content['page_index'] = $page_index;
+        }
+        $content['where'] = array(
+            'type'=>1,
+        );
+        $res = A('Callapi')->call_api('Inexposal.get_list', 
+                                    $content,
+                                    'text',
+                                  null);
+        $result = $this->deal_re_call_api($res);
+
+        $list = array();
+        if($result)
+        {
+            if(200 == $result['status_code'])
+            {
+                if(isset($result['content']['list'])
+                && isset($result['content']['record_count']))
+                {
+                    $list   = $result['content']['list'];   
+                    $this->assign('list', $list);     
+                    $record_count = $result['content']['record_count'];
+                    $this->assign('record_count', $record_count);
+                    $this->get_page($record_count, 10);
+                }
+            }
+        }
+        
+        $this->display();
+    }
+}
