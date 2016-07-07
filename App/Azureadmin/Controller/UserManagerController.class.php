7562f63330af90dba0a5234ea02f38a77361ab39
jinjiacun123 <jinjiacuncucu@sina.com>
2016-07-07 14:17:17 +0800

    修改:         App/Azureadmin/Controller/UserManagerController.class.php

--- App/Azureadmin/Controller/UserManagerController.class.php
+++ App/Azureadmin/Controller/UserManagerController.class.php
@@ -496,5 +496,334 @@ class UserManagerController extends BaseController{
 	   echo json_encode($out);
 	   exit();
 	}
+
+	/**
+	功能：重置用户密码
+
+	参数:
+	@@input
+	@param $uid int 用户id
+	@param $pwd string 密码
+	@param $repwd string 确认密码
+	*/
+	public function ResetUserPwd(){
+	       if(I('post.pwd') <> I('post.repwd')){
+	          header('Content-type:text/json');
+		  $out = array('res'=>502);
+	          echo json_encode($out);
+	          exit();
+	       }
+
+	       $content = array(
+	          'where' => array(),
+		  'data'  => array(),
+	       );
+	       //???
+	}
 	
+	/**
+	功能：修改密码
+
+	参数：
+	@@input
+	@param $OldPassword 旧密码
+	@param $NewPassword 新密码
+	*/	
+	public function SavePassword(){
+	 $com_admin_info = array();
+	 $content['AdminId'] = session('AdminId');
+	 $result = $this->_call('ComAdmin.get_info_by_key', $content);
+	 unset($content);
+
+	 if($result){
+	   if($result['status_code'] == 200){
+	     $com_admin_info = $result['content'];	 
+	   }
+	 }
+	 unset($result);
+
+	 if($com_admin_info['Password'] <> md5(I('post.OldPassword'))){
+	   header('Content-type:text/json');
+	   $out = array('msg'=>'原登陆密码错误.');
+	   echo json_encode($out);
+	   exit();
+	 }
+
+	 $content['data']['Password'] = I('post.NewPassword');
+	 $content['where']['AdminId'] = session('AdminId');
+	 $result = $this->_call('ComAdmin.update', $content);
+	 unset($content);
+	 if($result){
+	   if($result['status_code'] == 200
+	   && $result['content']['is_success'] == 0){
+	      header('Content-type:text/json');
+	      $out = array('msg'=>'修改成功.');
+	      echo json_encode($out);
+	      exit();
+	   }
+	 }
+	 
+	 header('Content-type:text/json');
+	 $out = array('msg'=>'修改失败.');
+	 echo json_encode($out);
+	 exit();
+	}
+
+	/*-----------------------分析师个人信息管理-----------------------------------------*/
+	
+	public function Bind_ComAdminLTE(){
+	   //获取分析师账号信息
+	   $com_admin_info = array();
+	   $content['AdminId'] = session('AdminId');
+	   $result = $this->_call('ComAdmin.get_info_by_key', $content);
+	   unset($content);
+	   if($result){
+	     if($resul['status_code'] == 200){
+	       $com_admin_info = $resul['content'];
+	     }
+	   }
+	   unset($result);
+
+	   //获取角色
+	   $com_role_info = array();
+	   $role_name = '';
+	   $content['RoleId'] = session('RoleId');
+	   $result = $this->_call('ComRole.get_info_by_key', $content);
+	   unset($content);
+	   if($result){
+	     if($result['status_code'] == 200){
+	       $com_role_info = $result['content'];
+	       $role_name = $com_role_info['RoleName'];
+	     }
+	   }
+	   unset($result);
+	   
+	   //所属直播室
+	   $com_room_list = array();
+	   $room_name_list = array();
+	   $room_names = '';
+	   $content['where']['RoomTeacher'] = array('like', '$'.session('AdminId').'$');
+	   $result = $this->_call('ComRoom.get_list', $content);
+	   unset($content);
+	   if($result){
+	     if($result['status_code']){	     
+	       $com_room_list = $result['content']['list'];	       
+	     }
+	   }
+	   unset($result);
+
+	   if(count($com_room_list) > 0){
+	    foreach($com_room_list as $v){
+	      $com_name_list = $v['RoomName'];
+	    }   
+	    unset($v);
+	    $room_names = implode(',', $com_name_list);
+	    unset($com_name_list);
+	    $room_names .= ',';
+	   }
+
+	   //分析师个人信息
+	   $com_admin_lte_info = array();
+	   $content['AdminId'] = session('AdminId');
+	   $result = $this->_call('ComAdminLTE.get_info', $content);
+	   unset($content);
+	   if($result){
+	     if($result['status_code'] == 200){
+	       $com_admin_lte_info = $result['content'];
+	     }
+	   }
+
+	   header('Content-type:text/json');
+	   $out = array(
+	   	'Adavatar'=>$com_admin['Adavatar'],
+		'ComAdminLTE' => array(
+		   lblAdminId = comAdmin.AdminId.ToString(),
+                        'lblAdminUserName' => $com_admin_info['AdminUserName'],
+                        'lblRoleName'      => $com_role_info['RoleName'],
+                        'lblRoomName'      => $com_room_info['RoomName'],
+                        //lblPower'        => lblPower,
+                        'lblAdminName'     => $com_admin_info['AdminName'],
+                        'lblComTLevel'     => $com_admin_lte_info['ComTLevel'],
+                        'lblComTStyle'     => $com_admin_lte['ComTStyle'] == '' ? "" : $com_admin_lte_info['ComTStyle'],
+                        'lblComTIntro'     => $com_admin_lte['ComTIntro'] == '' ? "" : $com_admin_lte_info['ComTIntro']
+		)
+	   );
+	   echo json_encode($out);
+	   exit();
+	}
+	
+
+	public function Bind_Edit_ComAdminLTE(){
+	  $id = I('post.id');
+	  //获取账号信息
+	  $com_admin_info = array();
+	  $content['AdminId'] = $id;
+	  $result = $this->_call('ComAdmin.get_info_by_key', $content);
+	  unset($content);
+	  if($result){
+	    if($result['status_code'] == 200){
+	       $com_admin_info = $result['content'];
+	    }
+	  }
+	  unset($result);
+
+	  //获取分析师个人信息
+	  $com_admin_lte_info = array();
+	  $content['ComAdminId'] = session('AdminId');
+	  $result = $this->_call('ComAdminLTE.get_info', $content);
+	  unset($content);
+	  if($result){
+	    if($result['status_code'] == 200){
+	      $com_admin_lte_info = $result['content'];
+	    }
+	  }
+	  unset($result);
+
+	  $html = "<div id=\"div_edit\" class=\"widget-box\">
+                <div class=\"widget-title\">
+                    <span class=\"icon\">
+                        <i class=\"icon-align-justify\"></i>
+                    </span>
+                    <h5>编辑</h5>
+                </div>
+                <div class=\"widget-content nopadding\">
+                    <form id=\"form_edit\" action=\"\" method=\"post\"
+                        class=\"form-horizontal\">
+                        <div class=\"control-group class1\">
+                            <img id=\"Adavatar\" src=\"{0}\" /><br/><br/>
+                                              <span class=\"uploadtips\">* 图片的尺寸必须是 136px * 136px</span>
+                            <div class=\"div-file\">
+                               <input type=\"file\" id=\"imgfile\" />
+                            </div>
+                        </div>
+                        <div class=\"control-group\">
+                            <label class=\"control-label\">分析师姓名</label>
+                            <div class=\"controls\">
+                                <input id=\"AdminName\" name=\"AdminName\" type=\"text\" value=\"{1}\" >
+                            </div>
+                        </div>
+                        <div class=\"control-group\">
+                            <label class=\"control-label\">分析师等级</label>
+                            <div class=\"controls\">
+                                <select id=\"ComTLevel\" name=\"ComTLevel\" class=\"s-auto\">
+                 {2}
+                                </select>
+            
+                            </div>
+                        </div>
+                        <div class=\"control-group\">
+                            <label class=\"control-label\">分析师风格</label>
+                            <div class=\"controls\">
+                                <input id=\"ComTStyle\" name=\"ComTStyle\" type=\"text\" value=\"{3}\" >
+                            </div>
+                        </div>
+                        <div class=\"control-group\">
+                            <label class=\"control-label\">分析师格言</label>
+                            <div class=\"controls\">
+                                <input id=\"ComTIntro\" name=\"ComTIntro\" type=\"text\" value=\"{4}\" >
+                            </div>
+                        </div>
+                        <div class=\"form-actions\">
+                            <button type=\"button\" onclick=\"SaveComAdminLTE('{5}');\" class=\"btn btn-primary\">保存</button>
+                        </div>
+                    </form>
+                </div>
+                        </div>";
+
+	    //替换
+	    $html = str_replace('{0}', $com_admin_info['Adavatar'], $html);//comAdmin.Adavatar
+	    $html = str_replace('{1}', $com_admin_info['AdminName'], $html);//comAdmin.AdminName
+	    $html = str_replace('{2}', '', $html);//GetStateOptions<EnumAnalystRank>(comAdminLTE.ComTLevel == null ? EnumAnalystRank.初级分析师.ToString("D") : comAdminLTE.ComTLevel),
+	    $html = str_replace('{3}', $com_admin_lte_info['ComTStyle'], $html);//comAdminLTE.ComTStyle == null ? "" : comAdminLTE.ComTStyle	   
+	    $html = str_replace('{4}', '', $html);//comAdminLTE.ComTIntro == null ? "" : comAdminLTE.ComTIntro,	    
+	    $html = str_replace('{5}', $id, $html);//id
+
+	    header('Content-type:text/json');
+	    $out = array(
+	      'html'=> $html
+	    );
+	    echo json_encode($out);
+	    exit();
+	}
+
+	public function SaveComAdminLTE(){
+	    //更新账号信息
+	    $com_admin_info = array();
+	    $content['where']['AdminId'] = I('post.AdminId');
+	    $content['data']['AdminName'] = I('post.AdminName');
+	    $content['data']['Adavatar'] = I('post.Adavatar');
+	    $result = $this->_call('ComAdmin.update', $content);
+	    unset($content);
+	    if($result){
+	      if($result['status_code'] == 200
+	      && $result['content']['is_success'] == 1){
+	        header('Content-type:text/json');
+		$out = array('res'=>-5001);
+		echo json_encode($out);
+	        exit();
+	      }
+	   }
+	   unset($result);
+	    
+	   //更新分析师信息
+	   //判定是否存在此账号的分析师
+	   $is_exists = False;
+	   $content['ComAdminId'] = I('post.AdminId');
+	   $result = $this->_call('ComAdminLTE.is_exist', $content);
+	   unset($content);
+	   if($result){
+	     if($result['status_code'] == 200
+	     && $result['content']['is_exists'] == 0){
+	       $is_exists = True;
+	     }
+	   }
+	   unset($result);
+
+	   if($is_exists){
+	      //更新
+	      $content = array(
+	         'where' => array('ComAdminId'=>I('post.AdminId')),
+		 'data'  => array(
+		   'ComTLevel' => urlencode(I("post.ComTLevel"));
+                   'ComTStyle' => urlencode(I("post.ComTStyle"));
+                   'ComTIntr'  => urlencode(I("post.ComTIntro"));
+		 )
+	      ); 
+	      $result = $this->_call('ComAdminLTE.update', $content);
+	      unset($content);
+	      if($result){
+	       if($result['status_code'] == 200
+	       && $result['content']['is_success'] == 1){
+	          header('Content-type:text/json');
+		  $out = array('res'=>-5001);
+	          echo json_encode($out);
+	       	  exit();
+	       }
+	     }
+	   }	
+	   else{
+	     //新增
+	     $content = array(
+
+	     );	      
+	     $result = $this->_call('ComAdminLTE.add', $content);
+	     unset($content);
+	     if($result){
+	       if($result['status_code'] == 200
+	       && $result['content']['is_success'] == 1){
+	          header('Content-type:text/json');
+		  $out = array('res'=>-5002);
+	          echo json_encode($out);
+	       	  exit();
+	       }
+	     }
+	   }
+
+	   header('Content-type:text/json');
+	   $out = array('res'=>1);
+	   echo json_encode($out);
+	   exit();
+	}
+	
+	/*-----------------------分析师个人信息管理-----------------------------------------*/
 }
