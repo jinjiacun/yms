487bd8f8aafdcb96f2b2057c9b4293080d05518a
jinjiacun123 <jinjiacuncucu@sina.com>
2016-07-14 20:03:18 +0800

    删除:         App/Azureadmin/Controller/ComManagerController.class.php 	修改:         App/Azureadmin/Controller/ComMessageController.class.php 	删除:         App/Azureadmin/View/ComMessager/ComMessager.html

--- App/Azureadmin/Controller/ComMessageController.class.php
+++ App/Azureadmin/Controller/ComMessageController.class.php
@@ -4,10 +4,58 @@ use Azureadmin\Controller;
 include_once(dirname(__FILE__).'/BaseController.class.php');
 
 class ComMessageController extends BaseController{
+    public function _initialize()
+    {
+        parent::_initialize();
+        parent::get_dictionary();
+        if(null == session('AdminName')
+        || ''   == session('AdminName'))
+        {
+            $this->redirect('/Azureadmin/Login/index');
+        }
+    }
 
-	public function ComMessager()
+	public function ComMessage()
 	{
 		$this->assign('menu_index', 20);
+        
+        $page_index = 1;
+        $page_size  = 20;
+        $content    = array();
+        if(I('get.p'))
+        {
+            $page_index = I('get.p');
+            $content['page_size'] = $page_size;
+            $content['page_index'] = $page_index;
+        }
+        else
+        {
+            $content['page_size'] = $page_size;
+            $content['page_index'] = $page_index;
+        }
+        
+        if(session('ComId')>0){
+            $content['where']['ComId'] = session('ComId');
+        }
+
+        $content['where']['CMState'] = 1;
+        
+        $result = $this->_call('ComMessage.get_list', $content);
+        unset($content);
+        $list = array();
+        if($result)
+            {
+                if($result['status_code'] == 200)
+                    {
+                        $list   = $result['content']['list'];
+                        $this->assign('list', $list);     
+                        $record_count = $result['content']['record_count'];
+                        $this->assign('record_count', $record_count);
+                        $this->assign('page', $this->get_page_by_custom(C('controller').'/CompanyManage/GetTable', 1, $record_count, $page_size));
+                    }
+            }
+        unset($result);
+        
 		$this->display();
 	}
 
